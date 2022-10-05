c8e470fd73f3228e6830f3f81a3c777ae7f474d3
Akobir Tursunov <akobirtursunov30@gmail.com>
2022-10-05 17:30:18 +0500

    change  a little

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//    Navbar Effect
+
 const header = document.querySelector("#header");
 document.addEventListener("scroll", (e) => {
   if (window.scrollY > 668) {
@@ -5,4 +7,43 @@ document.addEventListener("scroll", (e) => {
   } else {
     header.classList.remove("header-white");
   }
+});
+
+// auto typing
+
+let txt = document.querySelector(".auto-effect-1");
+let words = [
+  "Developer.",
+  "not Freelancer.",
+  "not Designer.",
+  "Alhamdulillah, Muslim.",
+];
+let wordIndex = 0;
+let letterIndex = 0;
+
+function typing() {
+  if (letterIndex < words[wordIndex].length) {
+    txt.innerHTML += words[wordIndex].charAt(letterIndex++);
+    setTimeout(typing, 200);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  // let eraseIndex = currentWord.length;
+  if (letterIndex > 0) {
+    txt.innerHTML = words[wordIndex].substring(0, letterIndex - 1);
+    letterIndex--;
+    setTimeout(erase, 100);
+  } else {
+    wordIndex++;
+    if (wordIndex >= words.length) wordIndex = 0;
+    setTimeout(typing, 1000);
+  }
+}
+
+typing();
+document.addEventListener("DOMContentLoaded", () => {
+  if (letterIndex.length) setTimeout(typing, 5000);
 });
